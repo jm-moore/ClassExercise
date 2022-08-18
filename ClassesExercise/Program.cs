28d6fbf7820ecb827df5106719bf42f0a9484726
jm-moore <jmmoore9358@gmail.com>
2022-08-18 11:27:19 -0500

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,8 @@
+﻿using ClassesExercise;
+
+Car myCar = new Car() 
+{ Make = "Ford", Model = "Taurus", Year = 2015};
+
+Console.WriteLine($"Make: {myCar.Make}");
+Console.WriteLine($"Model: {myCar.Model}");
+Console.WriteLine($"Year: {myCar.Year}");
